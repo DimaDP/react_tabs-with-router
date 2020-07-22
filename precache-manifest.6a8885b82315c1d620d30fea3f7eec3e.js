ad6bee9a08fd1d55c22b99527e107206268b20d6
DimaDP <dimadp95@gmail.com>
2020-07-22 14:20:30 +0300

    Updates

---
+++ precache-manifest.6a8885b82315c1d620d30fea3f7eec3e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42a42c9cbb113959d7ee5f82c23d18a0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "319f41343ba92733e7dc",
+    "url": "./static/css/main.24ff1b7e.chunk.css"
+  },
+  {
+    "revision": "8bd750a2b269f29ca803",
+    "url": "./static/js/2.f1b56fe3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.f1b56fe3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "319f41343ba92733e7dc",
+    "url": "./static/js/main.4b8d2c7a.chunk.js"
+  },
+  {
+    "revision": "12bee12ab4b288faf1d5",
+    "url": "./static/js/runtime-main.108729f7.js"
+  }
+]);
